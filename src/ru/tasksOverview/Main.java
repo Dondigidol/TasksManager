21232df2abb83951fd03d65898dbfc7376545129
dondigidol <pillar_box@bk.ru>
2018-04-07 19:54:05 +0300

    главный класс

---
+++ src/ru/tasksOverview/Main.java
@@ -0,0 +1,31 @@
+package ru.tasksOverview;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			primaryStage.setTitle("Задачи менеджера на день");
+			
+			Parent layout = FXMLLoader.load(getClass().getResource("view/tasksOverview.fxml"));
+			
+			
+			Scene scene = new Scene(layout);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
